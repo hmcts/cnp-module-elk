0d7d5b43f61afa3cd88c247003878bb191194029
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-08-08 15:47:14 +0100

    added installation of logstash

---
+++ src/packer_images/install_logstash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the PGP Key
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-6.x.list
+
+apt-get update
+if [ -z "$LOGSTASH_VERSION" ]; then
+    echo "Installing the latest Elasticsearch version"
+    apt-get install logstash
+else
+    echo "Installing Logstash version $LOGSTASH_VERSION"
+    apt-get install logstash=$LOGSTASH_VERSION
+fi
+
+mv logstash.conf /etc/logstash/logstash.yml

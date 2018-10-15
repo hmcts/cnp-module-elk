f35e8eecdbf505c7db6115032260e77c591bc371
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-10-15 13:31:09 +0100

    disabled kibana

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_template_deployment" "elastic-iaas" {
 
     esVersion         = "6.3.0"
     xpackPlugins      = "No"
-    kibana            = "Yes"
+    kibana            = "No"
 
     vmHostNamePrefix = "${var.product}-"
 

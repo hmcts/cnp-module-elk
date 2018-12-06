151f238eae57392a253df23eea8ef3f84150d9cf
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-12-06 14:40:01 +0000

    Implement cluster health alert

---
+++ alerts.tf
@@ -0,0 +1,23 @@
+resource "azurerm_template_deployment" "alert_cluster_health" {
+  name                = "alert_cluster_health"
+  template_body       = "${file("${path.module}/templates/alert.json")}"
+  resource_group_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
+  deployment_mode     = "Incremental"
+
+  parameters = {
+    workspaceName = "${data.azurerm_log_analytics_workspace.log_analytics.name}"
+    EmailRecipients = "ccd-devops@hmcts.net"
+    DisplayNameOfSearch = "Cluster health is not green"
+    UniqueNameOfSearch = "Cluster-unhealthy"
+    Description = "Checks that status_s for the healthcheck is != green"
+    SearchQuery = "es_health_CL | where status_s != \"green\""
+    Severity = "Critical"
+    TimeWindow = "10"
+    AlertFrequency = "5"
+    AggregateValueOperator = "gt"
+    AggregateValue = 0
+    TriggerAlertCondition = "Total"
+    TriggerAlertOperator = "gt"
+    TriggerAlertValue = "0"
+  }
+}

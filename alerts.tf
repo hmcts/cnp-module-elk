3024ea8df1566a208b5b4aceb4111f6b99e77432
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-12-10 15:04:24 +0000

    Alert DevOps with Red cluster status

--- alerts.tf
+++ alerts.tf
@@ -1,3 +1,14 @@
+module "elastic_devops_action_group" {
+  source = "git@github.com:hmcts/cnp-module-action-group"
+  location = "global"
+  env = "${var.env}"
+  resourcegroup_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
+  action_group_name = "ElasticSearch_DevOps_Prod"
+  short_name = "es-do-prod"
+  email_receiver_name = "Elasticsearch Alerts (DevOps - Prod)"
+  email_receiver_address = "ccd-elasticsearch-health-red@hmcts-devops.pagerduty.com"
+}
+
 module "elastic_ccd_action_group" {
   source = "git@github.com:hmcts/cnp-module-action-group"
   location = "global"
@@ -9,8 +20,8 @@ module "elastic_ccd_action_group" {
   email_receiver_address = "CCD_DevOps@hmcts.net"
 }
 
-resource "azurerm_template_deployment" "alert_cluster_health" {
-  name                = "alert_cluster_health"
+resource "azurerm_template_deployment" "alert_cluster_health_not_green" {
+  name                = "alert_cluster_health_not_green"
   template_body       = "${file("${path.module}/templates/alert.json")}"
   resource_group_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
   deployment_mode     = "Incremental"
@@ -23,6 +34,31 @@ resource "azurerm_template_deployment" "alert_cluster_health" {
     Description = "Checks that status_s for the healthcheck is != green"
     SearchQuery = "es_health_CL | where status_s != \"green\""
     Severity = "warning"
+    TimeWindow = "10"
+    AlertFrequency = "5"
+    AggregateValueOperator = "gt"
+    AggregateValue = "0"
+    TriggerAlertCondition = "Total"
+    TriggerAlertOperator = "gt"
+    TriggerAlertValue = "0"
+  }
+}
+
+resource "azurerm_template_deployment" "alert_cluster_health_red" {
+  count               = "${var.env == "prod" ? 1 : 0}"
+  name                = "alert_cluster_health_red"
+  template_body       = "${file("${path.module}/templates/alert.json")}"
+  resource_group_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
+  deployment_mode     = "Incremental"
+
+  parameters = {
+    workspaceName = "${data.azurerm_log_analytics_workspace.log_analytics.name}"
+    ActionGroupName = "${module.elastic_devops_action_group.action_group_name}"
+    DisplayNameOfSearch = "Cluster health is RED"
+    UniqueNameOfSearch = "Cluster-down"
+    Description = "Checks that status_s for the healthcheck is == red"
+    SearchQuery = "es_health_CL | where status_s == \"red\""
+    Severity = "critical"
     TimeWindow = "10"
     AlertFrequency = "5"
     AggregateValueOperator = "gt"

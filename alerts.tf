5d065c2c5c3625702754c0fa32f92b3976eee559
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-12-10 15:45:33 +0000

    Make queries and alerts $env specific
    nonprod has multiple envs reporting, so we need to add differentiation.
    deadletter queue doesn't have $env specific info.  So will report for
    all subscription envs.  So should be fine on prod.

--- alerts.tf
+++ alerts.tf
@@ -3,9 +3,9 @@ module "elastic_devops_action_group" {
   location = "global"
   env = "${var.env}"
   resourcegroup_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
-  action_group_name = "ElasticSearch_DevOps_Prod"
+  action_group_name = "ElasticSearch_DevOps_${var.env}"
   short_name = "es-do-prod"
-  email_receiver_name = "Elasticsearch Alerts (DevOps - Prod)"
+  email_receiver_name = "Elasticsearch Alerts (DevOps - ${var.env})"
   email_receiver_address = "ccd-elasticsearch-health-red@hmcts-devops.pagerduty.com"
 }
 
@@ -14,14 +14,14 @@ module "elastic_ccd_action_group" {
   location = "global"
   env = "${var.env}"
   resourcegroup_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
-  action_group_name = "ElasticSearch_CCD_DevOps"
+  action_group_name = "ElasticSearch_CCD_DevOps_${var.env}"
   short_name = "es-ccd-ops"
-  email_receiver_name = "Elasticsearch Alerts (CCD)"
+  email_receiver_name = "Elasticsearch Alerts (CCD) - ${var.env}"
   email_receiver_address = "CCD_DevOps@hmcts.net"
 }
 
 resource "azurerm_template_deployment" "alert_cluster_health_not_green" {
-  name                = "alert_cluster_health_not_green"
+  name                = "alert_cluster_health_not_green_${var.env}"
   template_body       = "${file("${path.module}/templates/alert.json")}"
   resource_group_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
   deployment_mode     = "Incremental"
@@ -29,10 +29,10 @@ resource "azurerm_template_deployment" "alert_cluster_health_not_green" {
   parameters = {
     workspaceName = "${data.azurerm_log_analytics_workspace.log_analytics.name}"
     ActionGroupName = "${module.elastic_ccd_action_group.action_group_name}"
-    DisplayNameOfSearch = "Cluster health is not green"
-    UniqueNameOfSearch = "Cluster-unhealthy"
-    Description = "Checks that status_s for the healthcheck is != green"
-    SearchQuery = "es_health_CL | where status_s != \"green\" or error_type_s != \"\""
+    DisplayNameOfSearch = "Cluster health is not green on ${var.env}"
+    UniqueNameOfSearch = "Cluster-unhealthy-${var.env}"
+    Description = "Checks that status_s for the healthcheck is != green on ${var.env}"
+    SearchQuery = "es_health_CL | where (status_s != \"green\" and cluster_s == \"${azurerm_resource_group.elastic-resourcegroup.name}\") or error_type_s != \"\""
     Severity = "warning"
     TimeWindow = "10"
     AlertFrequency = "5"
@@ -46,7 +46,7 @@ resource "azurerm_template_deployment" "alert_cluster_health_not_green" {
 
 resource "azurerm_template_deployment" "alert_cluster_health_red" {
   count               = "${var.env == "prod" ? 1 : 0}"
-  name                = "alert_cluster_health_red"
+  name                = "alert_cluster_health_red_${var.env}"
   template_body       = "${file("${path.module}/templates/alert.json")}"
   resource_group_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
   deployment_mode     = "Incremental"
@@ -54,10 +54,10 @@ resource "azurerm_template_deployment" "alert_cluster_health_red" {
   parameters = {
     workspaceName = "${data.azurerm_log_analytics_workspace.log_analytics.name}"
     ActionGroupName = "${module.elastic_devops_action_group.action_group_name}"
-    DisplayNameOfSearch = "Cluster health is RED"
-    UniqueNameOfSearch = "Cluster-down"
-    Description = "Checks that status_s for the healthcheck is == red"
-    SearchQuery = "es_health_CL | where status_s == \"red\" or error_type_s != \"\""
+    DisplayNameOfSearch = "Cluster health is RED ${var.env}"
+    UniqueNameOfSearch = "Cluster-down-${var.env}"
+    Description = "Checks that status_s for the healthcheck is == red on ${var.env}"
+    SearchQuery = "es_health_CL | where (status_s == \"red\" and cluster_s == \"${azurerm_resource_group.elastic-resourcegroup.name}\") or error_type_s != \"\""
     Severity = "critical"
     TimeWindow = "10"
     AlertFrequency = "5"

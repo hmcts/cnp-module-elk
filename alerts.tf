25035baadfd9fa1831c08b49aefc3943761ba1c8
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-12-10 11:31:37 +0000

    Add dead letter queue alert

--- alerts.tf
+++ alerts.tf
@@ -32,3 +32,27 @@ resource "azurerm_template_deployment" "alert_cluster_health" {
     TriggerAlertValue = "0"
   }
 }
+
+resource "azurerm_template_deployment" "alert_dead_letter_queue" {
+  name                = "alert_dead_letter_queue"
+  template_body       = "${file("${path.module}/templates/alert.json")}"
+  resource_group_name = "${data.azurerm_log_analytics_workspace.log_analytics.resource_group_name}"
+  deployment_mode     = "Incremental"
+
+  parameters = {
+    workspaceName = "${data.azurerm_log_analytics_workspace.log_analytics.name}"
+    ActionGroupName = "${module.elastic_action_group.action_group_name}"
+    DisplayNameOfSearch = "ElasticSearch deadletter queue is not empty"
+    UniqueNameOfSearch = "Cluster-deadletters_present"
+    Description = "Checks that status_s for the healthcheck is != green"
+    SearchQuery = "es_indices_CL | where index_s == \".logstash_dead_letter\" and docs_count_s != \"0\""
+    Severity = "warning"
+    TimeWindow = "10"
+    AlertFrequency = "5"
+    AggregateValueOperator = "gt"
+    AggregateValue = "0"
+    TriggerAlertCondition = "Total"
+    TriggerAlertOperator = "gt"
+    TriggerAlertValue = "0"
+  }
+}

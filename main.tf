04d5ff7db81f16aa458b87ce85e5426937fc6beb
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2019-10-08 18:00:03 +0100

    RDM-6000 - ELK change for applications access in AKS

--- main.tf
+++ main.tf
@@ -97,6 +97,11 @@ data "azurerm_virtual_network" "core_infra_vnet" {
   resource_group_name  = "core-infra-${var.env}"
 }
 
+data "azurerm_virtual_network" "aks_core_vnet" {
+  name                 = "core-${var.env}-vnet"
+  resource_group_name  = "aks-infra-${var.env}-rg"
+}
+
 data "azurerm_subnet" "elastic-subnet" {
   name                 = "elasticsearch"
   virtual_network_name = "${data.azurerm_virtual_network.core_infra_vnet.name}"
@@ -111,6 +116,18 @@ data "azurerm_subnet" "apps" {
 
 data "azurerm_subnet" "aks" {
   name                 = "aks"
+  virtual_network_name = "${data.azurerm_virtual_network.core_infra_vnet.name}"
+  resource_group_name  = "${data.azurerm_virtual_network.core_infra_vnet.resource_group_name}"
+}
+
+data "azurerm_subnet" "aks-00" {
+  name                 = "aks-00"
+  virtual_network_name = "${data.azurerm_virtual_network.core_infra_vnet.name}"
+  resource_group_name  = "${data.azurerm_virtual_network.core_infra_vnet.resource_group_name}"
+}
+
+data "azurerm_subnet" "aks-01" {
+  name                 = "aks-01"
   virtual_network_name = "${data.azurerm_virtual_network.core_infra_vnet.name}"
   resource_group_name  = "${data.azurerm_virtual_network.core_infra_vnet.resource_group_name}"
 }
@@ -194,7 +211,8 @@ resource "azurerm_network_security_rule" "apps_rule" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "9200"
-  source_address_prefixes      = ["${data.azurerm_subnet.apps.address_prefix}","${data.azurerm_subnet.aks.address_prefix}"]
+  source_address_prefixes      = ["${data.azurerm_subnet.apps.address_prefix}","${data.azurerm_subnet.aks.address_prefix}",
+    "${data.azurerm_subnet.aks-00.address_prefix}", "${data.azurerm_subnet.aks-01.address_prefix}"]
   destination_application_security_group_ids = ["${data.azurerm_application_security_group.data_asg.id}"]
   resource_group_name         = "${azurerm_resource_group.elastic-resourcegroup.name}"
   network_security_group_name = "${data.azurerm_network_security_group.cluster_nsg.name}"

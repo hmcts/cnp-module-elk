fbcedc190bcbc69da548057bd5f83789551d6327
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2019-10-09 12:52:21 +0100

    RDM-6000 - fixing provider name

--- aks.tf
+++ aks.tf
@@ -4,20 +4,20 @@ provider "azurerm" {
 }
 
 data "azurerm_virtual_network" "aks_core_vnet" {
-  provider             = "aks-infra"
+  provider             = "azurerm.aks-infra"
   name                 = "core-${var.env}-vnet"
   resource_group_name  = "aks-infra-${var.env}-rg"
 }
 
 data "azurerm_subnet" "aks-00" {
-  provider             = "aks-infra"
+  provider             = "azurerm.aks-infra"
   name                 = "aks-00"
   virtual_network_name = "${data.azurerm_virtual_network.aks_core_vnet.name}"
   resource_group_name  = "${data.azurerm_virtual_network.aks_core_vnet.resource_group_name}"
 }
 
 data "azurerm_subnet" "aks-01" {
-  provider             = "aks-infra"
+  provider             = "azurerm.aks-infra"
   name                 = "aks-01"
   virtual_network_name = "${data.azurerm_virtual_network.aks_core_vnet.name}"
   resource_group_name  = "${data.azurerm_virtual_network.aks_core_vnet.resource_group_name}"

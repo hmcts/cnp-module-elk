48f0b07d669052e389f0b6d1b416affc425161f7
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-05-11 15:32:35 +0100

    fix

--- aks.tf
+++ aks.tf
@@ -1,18 +1,18 @@
 data "azurerm_virtual_network" "aks_core_vnet" {
-  provider            = "azurerm.aks-infra"
+  provider            = azurerm.aks-infra
   name                = "core-${local.env}-vnet"
   resource_group_name = "aks-infra-${local.env}-rg"
 }
 
 data "azurerm_subnet" "aks-00" {
-  provider             = "azurerm.aks-infra"
+  provider             = azurerm.aks-infra
   name                 = "aks-00"
   virtual_network_name = "${data.azurerm_virtual_network.aks_core_vnet.name}"
   resource_group_name  = "${data.azurerm_virtual_network.aks_core_vnet.resource_group_name}"
 }
 
 data "azurerm_subnet" "aks-01" {
-  provider             = "azurerm.aks-infra"
+  provider             = azurerm.aks-infra
   name                 = "aks-01"
   virtual_network_name = "${data.azurerm_virtual_network.aks_core_vnet.name}"
   resource_group_name  = "${data.azurerm_virtual_network.aks_core_vnet.resource_group_name}"

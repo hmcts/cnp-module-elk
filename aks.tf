a480a2388a86ccf9738cfa23009ac2718608f3a3
Thomas Thornton <>
2021-05-14 10:53:33 +0100

    Revert "Cleanup"
    This reverts commit 6cc00e952e3dc049923fb708546fab765fcd2c13.

--- aks.tf
+++ aks.tf
@@ -22,18 +22,18 @@ locals {
   env = var.env == "sandbox" ? "sbox" : var.env
 }
 
-# resource "azurerm_network_security_rule" "aks_rule" {
-#   name                                       = "AKS_To_ES"
-#   description                                = "Allow AKS to access the ElasticSearch cluster"
-#   priority                                   = 215
-#   direction                                  = "Inbound"
-#   access                                     = "Allow"
-#   protocol                                   = "Tcp"
-#   source_port_range                          = "*"
-#   destination_port_range                     = "9200"
-#   source_address_prefixes                    = [data.azurerm_subnet.aks-00.address_prefix, data.azurerm_subnet.aks-01.address_prefix]
-#   destination_application_security_group_ids = [data.azurerm_application_security_group.data_asg.id]
-#   resource_group_name                        = azurerm_resource_group.elastic-resourcegroup.name
-#   network_security_group_name                = data.azurerm_network_security_group.cluster_nsg.name
-#   depends_on                                 = ["azurerm_template_deployment.elastic-iaas"]
-# }
+resource "azurerm_network_security_rule" "aks_rule" {
+  name                                       = "AKS_To_ES"
+  description                                = "Allow AKS to access the ElasticSearch cluster"
+  priority                                   = 215
+  direction                                  = "Inbound"
+  access                                     = "Allow"
+  protocol                                   = "Tcp"
+  source_port_range                          = "*"
+  destination_port_range                     = "9200"
+  source_address_prefixes                    = [data.azurerm_subnet.aks-00.address_prefix, data.azurerm_subnet.aks-01.address_prefix]
+  destination_application_security_group_ids = [data.azurerm_application_security_group.data_asg.id]
+  resource_group_name                        = azurerm_resource_group.elastic-resourcegroup.name
+  network_security_group_name                = data.azurerm_network_security_group.cluster_nsg.name
+  depends_on                                 = ["azurerm_template_deployment.elastic-iaas"]
+}

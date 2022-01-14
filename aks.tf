1867e864a3afc6eec7d0021399aaa80938490e75
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2022-01-14 11:58:42 +0000

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,7 +1,7 @@
 data "azurerm_virtual_network" "aks_core_vnet" {
   provider            = azurerm.aks-infra
-  name                = "cft-${local.env}-vnet"
-  resource_group_name = "cft-${local.env}-network-rg"
+  name                = var.env == "demo" ? "core-${local.env}-vnet" : "cft-${local.env}-vnet"
+  resource_group_name = var.env == "demo" ? "aks-infra-${local.env}-rg" : "cft-${local.env}-network-rg"
 }
 
 data "azurerm_subnet" "aks-00" {

55f7b0b527b3dc0fdc99ec71207adebce061e426
Darren Bromwich <darrenb@dwm779ym9d.broadband>
2022-04-21 11:02:31 +0100

    updated aks_core_vnet block so that demo now has the cft name and rg associated to it

--- aks.tf
+++ aks.tf
@@ -1,7 +1,7 @@
 data "azurerm_virtual_network" "aks_core_vnet" {
   provider            = azurerm.aks-infra
-  name                = var.env == "demo" || var.env == "prod" ? "core-${local.env}-vnet" : "cft-${local.env}-vnet"
-  resource_group_name = var.env == "demo" || var.env == "prod" ? "aks-infra-${local.env}-rg" : "cft-${local.env}-network-rg"
+  name                = var.env == "prod" ? "core-${local.env}-vnet" : "cft-${local.env}-vnet"
+  resource_group_name = var.env == "prod" ? "aks-infra-${local.env}-rg" : "cft-${local.env}-network-rg"
 }
 
 data "azurerm_subnet" "aks-00" {

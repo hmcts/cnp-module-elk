dbe4ceb5621c74504b0f377baf14c19ebeff7aeb
Darren Bromwich <darrenb@dwm779ym9d.broadband>
2022-04-21 12:35:39 +0100

    testing updated config aliases

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       source                = "hashicorp/azurerm"
       version               = ">= 2.58.0"
-      configuration_aliases = [azurerm.aks-infra, azurerm.mgmt]
+      configuration_aliases = [ aks-infra, mgmt ]
     }
   }
 }

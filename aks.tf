3f04b7a31d89cb9eda5b942b15e6ec872d703044
Darren Bromwich <darrenb@kainos.com>
2022-04-26 17:15:03 +0100

    test aks_core_vnet change

--- aks.tf
+++ aks.tf
@@ -2,7 +2,7 @@
 data "azurerm_virtual_network" "aks_core_vnet" {
   provider            = azurerm.aks-infra
   name                = var.env == "demo" ? "cft-${local.env}-vnet" || var.env == "prod" ? "core-${local.env}-vnet" : "cft-${local.env}-vnet"
-  resource_group_name = var.env == "demo" ? "cft-${local.env}-network-rg" ||var.env == "prod" ? "aks-infra-${local.env}-rg" : "cft-${local.env}-network-rg"
+  resource_group_name = var.env == "demo" ? "cft-${local.env}-network-rg" || var.env == "prod" ? "aks-infra-${local.env}-rg" : "cft-${local.env}-network-rg"
 }
 
 data "azurerm_subnet" "aks-00" {

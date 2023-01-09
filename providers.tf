19edefb24ddd2e0995c6384ba8ba2c130ef2450b
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-01-09 21:51:32 +0000

    update

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = ">= 2.58.0"
-      configuration_aliases = ["azurerm.aks-infra", "azurerm.mgmt"]
+      version               = ">= 3.38.0"
+      configuration_aliases = [azurerm.aks-infra, azurerm.mgmt]
     }
   }
 }

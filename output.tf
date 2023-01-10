679a27e354631403c1807ce17b0a0895922b8c63
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-01-10 12:33:33 +0000

    revert_output.tf

---
+++ output.tf
@@ -0,0 +1,23 @@
+output "loadbalancerManual" {
+  value = "${local.vNetLoadBalancerIp}"
+}
+
+output "loadbalancer" {
+  value = "${azurerm_template_deployment.elastic-iaas.outputs["loadbalancer"]}"
+}
+
+output "kibana" {
+  value = "${azurerm_template_deployment.elastic-iaas.outputs["kibana"]}"
+}
+
+output "jumpboxssh" {
+  value = "${azurerm_template_deployment.elastic-iaas.outputs["jumpboxssh"]}"
+}
+
+output "elastic_resource_group_name" {
+  value = "${azurerm_resource_group.elastic-resourcegroup.name}}"
+}
+
+output "elasticsearch_admin_password" {
+  value = "${local.securePassword}"
+}

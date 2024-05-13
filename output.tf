e9e198f5fbd544084e19c8c88a4b6a5082252795
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-05-13 11:19:45 +0100

    disable broken outputs

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "loadbalancer" {
-  value = azurerm_template_deployment.elastic-iaas.outputs["loadbalancer"]
-}
+# output "loadbalancer" {
+#   value = azurerm_template_deployment.elastic-iaas.outputs["loadbalancer"]
+# }
 
-output "kibana" {
-  value = azurerm_template_deployment.elastic-iaas.outputs["kibana"]
-}
+# output "kibana" {
+#   value = azurerm_template_deployment.elastic-iaas.outputs["kibana"]
+# }
 
-output "jumpboxssh" {
-  value = azurerm_template_deployment.elastic-iaas.outputs["jumpboxssh"]
-}
+# output "jumpboxssh" {
+#   value = azurerm_template_deployment.elastic-iaas.outputs["jumpboxssh"]
+# }
 
 output "elastic_resource_group_name" {
   value = "${azurerm_resource_group.elastic-resourcegroup.name}}"

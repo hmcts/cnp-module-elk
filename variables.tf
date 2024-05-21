97c7909f10445cd14274ce48c809049e19ff9f8a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-05-21 08:26:52 +0100

    update default

--- variables.tf
+++ variables.tf
@@ -42,7 +42,7 @@ variable "storageAccountType" {
 
 variable "dataStorageAccountType" {
   description = "disk storage account type"
-  default     = "Default"
+  default     = "Standard"
 }
 
 variable "vmSizeAllNodes" {

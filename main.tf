a142edd1e7462263cba0b21dbb7ccc08b186dde2
nicholas chang <nicholasc001@hotmail.com>
2024-08-13 17:03:01 +0100

    rebase from branch

--- main.tf
+++ main.tf
@@ -170,7 +170,7 @@ resource "azurerm_network_security_rule" "bastion_es_rule" {
   protocol                                   = "Tcp"
   source_port_range                          = "*"
   destination_port_range                     = "9200"
-  source_address_prefixes                    = var.subscription == "prod" || var.subscription == "ethosldata" ? ["10.11.8.32/27"] : ["10.11.72.32/27"]
+  source_address_prefixes                    = var.subscription == "prod" || var.subscription == "ethosldata" ? ["10.11.8.32/27"] : [ "10.11.72.32/27"]
   destination_application_security_group_ids = [data.azurerm_application_security_group.data_asg.id]
   resource_group_name                        = azurerm_resource_group.elastic-resourcegroup.name
   network_security_group_name                = data.azurerm_network_security_group.cluster_nsg.name
@@ -218,7 +218,7 @@ resource "azurerm_network_security_rule" "bastion_ssh_rule" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefixes     = var.subscription == "prod" || var.subscription == "ethosldata" ? ["10.11.8.32/27"] : ["10.11.72.32/27"]
+  source_address_prefixes     = var.subscription == "prod" || var.subscription == "ethosldata" ? ["10.11.8.32/27"] : [ "10.11.72.32/27"]
   destination_address_prefix  = data.azurerm_subnet.elastic-subnet.address_prefix
   resource_group_name         = azurerm_resource_group.elastic-resourcegroup.name
   network_security_group_name = data.azurerm_network_security_group.cluster_nsg.name
